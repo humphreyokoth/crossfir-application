67cad759c87800e0a34e037164e25f3d0fe921b1
humphreyokoth <humpasp@gmail.com>
2022-05-13 10:06:10 +0300

    JSON stringfy

--- src/controllers/workoutController.js
+++ src/controllers/workoutController.js
@@ -16,8 +16,20 @@ const getAllWorkouts = (req, res) => {
   };
   
   const createNewWorkout = (req, res) => {
-    const createdWorkout = workoutService.createNewWorkout();
-    res.send("Create a new workout");
+
+
+    const{body}= req;
+    if(!body.name ||!body.mode ||!body.equiment|| !body.exercises || !body.trainerTips){
+      return;
+    };
+    const newWorkout = {
+      name:body.name,
+      mode:body.mode,
+      equiment:body.equiment,
+      trainerTips:body.trainerTips
+    };
+    const createdWorkout = workoutService.createNewWorkout(newWorkout);
+    res.status(201).send({status:"OK",data:createNewWorkout});
   };
   
   const updateOneWorkout = (req, res) => {
--- src/database/Workout.js
+++ src/database/Workout.js
@@ -1,7 +1,23 @@
 const DB = require("./db.json");
+//Change JSON data to string format to be saved in DB.
+const{saveToDatabase} = require("./utils");
 
 const getAllWorkouts = ()=>{
     return DB.workouts;
 };
 
-module.exports = { getAllWorkouts};
+
+const createdWorkout =(newWorkout)=>{
+    const isAlreadyAdded = DB.workouts.findIndex((workout)=> workout.name === newWorkout.name)>-1;
+    if(isAlreadyAdded){
+        return;
+    }
+    DB.workouts.push(newWorkout);
+    saveToDatabase(DB);
+    return newWorkout;
+};
+
+module.exports = { 
+        getAllWorkouts,
+        createdWorkout
+};
