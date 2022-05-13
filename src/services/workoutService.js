c08da8eb26cbe1cdf4560aab5ebed53e1962f96f
humphreyokoth <humpasp@gmail.com>
2022-05-13 11:14:43 +0300

    new workout created

--- src/services/workoutService.js
+++ src/services/workoutService.js
@@ -1,18 +1,27 @@
+const {v4:uuid} = require("uuid");
+
 const Workout = require("../database/Workout")
 
 
 // Get all workouts service.
 const getAllWorkouts =()=>{
-    const allWorkouts = Workout.getAllWorkouts();
-return allWorkouts  ;
+   // const allWorkouts = Workout.getAllWorkouts();
+return DB.workouts ;
 };
 // Get one workout .
 const getOneWorkout=()=>{
     return;
 };
 
-const createNewWorkout =()=>{
-    return;
+const createNewWorkout =(newWorkout)=>{
+   const workoutTOInsert = {
+       ...newWorkout,
+       id:uuid(),
+       createdAt:new Date().toLocaleString("en-US", { timeZone: "EAT" }),
+       updatedAt:new Date().toLocaleString("en-US", { timeZone: "EAT" })
+}
+const createdWorkout = Workout.createNewWorkout(workoutTOInsert);
+    return createdWorkout;
 };
 
 const updateOneWorkout =()=>{
