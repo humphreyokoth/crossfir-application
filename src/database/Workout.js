1a367e7a31edf85a10fee6d0d77b962ff4f561ab
humphreyokoth <humpasp@gmail.com>
2022-05-16 16:46:05 +0300

    members and records

--- src/database/Workout.js
+++ src/database/Workout.js
@@ -1,66 +1,102 @@
+// In src/database/Workout.js
 const DB = require("./db.json");
-//Change JSON data to string format to be saved in DB.
-const{saveToDatabase} = require("./utils");
+const { saveToDatabase } = require("./utils");
 
-const getAllWorkouts = ()=>{
+const getAllWorkouts = () => {
+  try {
     return DB.workouts;
+  } catch (error) {
+    throw { status: 500, message: error };
+  }
 };
 
 const getOneWorkout = (workoutId) => {
+  try {
     const workout = DB.workouts.find((workout) => workout.id === workoutId);
     if (!workout) {
-      return;
+      throw {
+        status: 400,
+        message: `Can't find workout with the id '${workoutId}'`,
+      };
     }
     return workout;
-  };
-
-const createNewWorkout =(newWorkout)=>{
-    const isAlreadyAdded = DB.workouts.findIndex((workout)=> workout.name === newWorkout.name)>-1;
-    if(isAlreadyAdded){
-        
-        throw{
-            status:400,
-            message:`Workout with the name "${newWorkout.name}"already exists`,
-        };
-    }
- try{
-   DB.workouts.push(newWorkout);
-    saveToDatabase(DB);
-    return newWorkout;
-    }catch (error){
-        throw {status:500,message:error?.message|| error};
-    }
+  } catch (error) {
+    throw { status: error?.status || 500, message: error?.message || error };
+  }
 };
 
-const updateOneWorkout = (workoutId,changes)=>{
-    const indexForUpdate = DB.workouts.findIndex((workout)=> workout.id === workoutId);
-    if(indexForUpdate === -1){
-        return;
+const createNewWorkout = (newWorkout) => {
+  try {
+    const isAlreadyAdded =
+      DB.workouts.findIndex((workout) => workout.name === newWorkout.name) > -1;
+    if (isAlreadyAdded) {
+      throw {
+        status: 400,
+        message: `Workout with the name '${newWorkout.name}' already exists`,
+      };
     }
+    DB.workouts.push(newWorkout);
+    saveToDatabase(DB);
+    return newWorkout;
+  } catch (error) {
+    throw { status: error?.status || 500, message: error?.message || error };
+  }
+};
 
-    const updatedWorkout ={...DB.workouts[indexForUpdate],
-    ...changes,
-    updatedAt:new Date().toLocaleString("en-US",{timeZone:"EAT"})
+const updateOneWorkout = (workoutId, changes) => {
+  try {
+    const isAlreadyAdded =
+      DB.workouts.findIndex((workout) => workout.name === changes.name) > -1;
+    if (isAlreadyAdded) {
+      throw {
+        status: 400,
+        message: `Workout with the name '${changes.name}' already exists`,
+      };
+    }
+    const indexForUpdate = DB.workouts.findIndex(
+      (workout) => workout.id === workoutId
+    );
+    if (indexForUpdate === -1) {
+      throw {
+        status: 400,
+        message: `Can't find workout with the id '${workoutId}'`,
+      };
+    }
+    const updatedWorkout = {
+      ...DB.workouts[indexForUpdate],
+      ...changes,
+      updatedAt: new Date().toLocaleString("en-US", { timeZone: "UTC" }),
     };
     DB.workouts[indexForUpdate] = updatedWorkout;
     saveToDatabase(DB);
     return updatedWorkout;
-
+  } catch (error) {
+    throw { status: error?.status || 500, message: error?.message || error };
+  }
 };
-const  deleteOneWorkout = (workoutId)=>{
-    const indexForDeletion = DB.workouts.findIndex((workout)=>workout.id === workoutId);
-    if(indexForDeletion === -1){
-        return;
+
+const deleteOneWorkout = (workoutId) => {
+  try {
+    const indexForDeletion = DB.workouts.findIndex(
+      (workout) => workout.id === workoutId
+    );
+    if (indexForDeletion === -1) {
+      throw {
+        status: 400,
+        message: `Can't find workout with the id '${workoutId}'`,
+      };
     }
-    DB.workouts.splice(indexForDeletion,1);
+    DB.workouts.splice(indexForDeletion, 1);
     saveToDatabase(DB);
-}
+  } catch (error) {
+    throw { status: error?.status || 500, message: error?.message || error };
+  }
+};
 
-
-module.exports = { 
-    getAllWorkouts,
-    createNewWorkout,
-    getOneWorkout,
-    updateOneWorkout,
-    deleteOneWorkout,
+module.exports = {
+  getAllWorkouts,
+  createNewWorkout,
+  getOneWorkout,
+  updateOneWorkout,
+  deleteOneWorkout,
 };
